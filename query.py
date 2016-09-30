1d1ac8cb68f8891beb3eb65b89a022a72495e2f8
thang <thangdd@topica.edu.vn>
2016-09-30 20:41:38 +0700

    Complate Etl_siren version_01

--- query.py
+++ query.py
@@ -32,7 +32,20 @@ def process_json():
     for i in collection:
         if i == 'feedbacks' or i == 'call_logs':
             if i == 'feedbacks':
-                with open("./data/%s.json" % i, "wb") as f:
+                with open("./data/%s.json" % i, "w") as f:
+                    cursor = database[i].find({})
+                    for document in cursor:
+                        u = "{:%d/%m/%Y/%H/%M/%S}".format(document['updated_at'])
+                        c = "{:%d/%m/%Y/%H/%M/%S}".format(document['updated_at'])
+                        document['updated_at'] = time.mktime(
+                            datetime.datetime.strptime(u, "%d/%m/%Y/%H/%M/%S").timetuple())
+                        document['created_at'] = time.mktime(
+                            datetime.datetime.strptime(c, "%d/%m/%Y/%H/%M/%S").timetuple())
+                        document = json.dumps(document, default=json_util.default)
+                        f.write(document + '\n')
+                    f.close()
+            else:
+                with open("./data/%s.json" % i, "w") as f:
                     cursor = database[i].find({})
                     for document in cursor:
                         u = "{:%d/%m/%Y/%H/%M/%S}".format(document['updated_at'])
@@ -43,35 +56,27 @@ def process_json():
                             datetime.datetime.strptime(c, "%d/%m/%Y/%H/%M/%S").timetuple())
                         document = json_util.dumps(document)
                         document = json.dumps(document, default=json_util.default)
-                        f.write(document + "\n")
+                        f.write(document + '\n')
                     f.close()
-            # else:
-            #     with open("./data/%s.json" % i, "wb") as f:
-            #         cursor = database[i].find({})
-            #         for document in cursor:
-            #             u = "{:%d/%m/%Y/%H/%M/%S}".format(document['updated_at'])
-            #             c = "{:%d/%m/%Y/%H/%M/%S}".format(document['updated_at'])
-            #             document['updated_at'] = time.mktime(datetime.datetime.strptime(u, "%d/%m/%Y/%H/%M/%S").timetuple())
-            #             document['created_at'] = time.mktime(datetime.datetime.strptime(c, "%d/%m/%Y/%H/%M/%S").timetuple())
-            #             document = json_util.dumps(document)
-            #             document = json.dumps(document, default=json_util.default)
-            #             json.dump(f, document)
 
 
-# def check_file():
-#     file = glob.glob("./data/*")
-#     for f in file:
-#         if os.path.exists(f):
-#             if (float(os.path.getsize(f)) / (1024 * 1024)) >= 64:
-#                 os.system('split -b60m %s ./data/feedb' % f)
-#                 os.system('rm -rf %s' % f)
+def check_file():
+    file = glob.glob("./data/*")
+    for f in file:
+        if os.path.exists(f):
+            if (float(os.path.getsize(f)) / (1024 * 1024)) >= 64:
+                os.system('split -b60m %s ./data/feedbacks' % f)
+                os.system('rm -rf %s' % f)
 
 
-# def put_hadoop():
-#     os.system('hdfs dfs -put ./data/*  /user/hadoop/edumall/siren')
+def put_hadoop():
+    os.system('hdfs dfs -put ./data/*  /user/hadoop/edumall/siren')
 
 
 if __name__ == '__main__':
     get_database()
     getcoll()
     process_json()
+    check_file()
+    put_hadoop()
+
